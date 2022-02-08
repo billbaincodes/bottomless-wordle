d3636191e9f5de95bf2fea9fd363350a9b4547c1
bill <williamandrewbain@gmail.com>
2022-02-07 17:06:39 -0700

    start on broader game logic

--- app.js
+++ app.js
@@ -1,33 +1,70 @@
 console.log('js linked');
 
+// const wordChecker = require('./logic');
+import { wordChecker } from './logic.js';
 
-class Guess {
+// Init variables
+let answer = 'pleat'
+let guess = '';
+let attempt = 0;
+let submitBtn = document.getElementById("submit-btn");
+let wordField = document.getElementById("word-field")
 
-  answer
-  guess
+// Add submit listener
+submitBtn.addEventListener('click', (e) => {
 
+  if (attempt >= 4) return;
 
-  constructor(answer, guess){
+  console.log({ answer, guess });
 
-    this.answer = answer;
-    this.guess = guess;
+  // Check their word!
+  let result = wordChecker(answer, guess);
 
+  console.log({ result });
+
+  // Clear the guess input
+  wordField.innerText = '';
+
+  // Clear the guess
+  guess = ''
+
+  // Increment attempt number
+  attempt++
+
+});
+
+// Add word input listener
+wordField.addEventListener('keyup', (e) => {
+
+  // Update guess with value from input
+  guess = e.target.value.toUpperCase()
+
+  // Update the letters on the board
+  updateBoard();
+
+});
+
+// Update the board with the proposed guess' letters
+const updateBoard = () => {
+
+  // Split guess string into Arr
+  const guessArr = guess.split('')
+
+  // Capture spans matching attempt number
+  let attemptDiv = document.getElementById(`attempt_${attempt}`);
+
+  // Update each span with corresponding letter
+  for (let i = 0; i < attemptDiv.children.length; i++) {
+    attemptDiv.children[i].innerText = guessArr[i] || ' ';
   }
-
-  
 
 }
 
-let guess1 = new Guess('poodle', 'g')
+console.log(submitBtn, wordField);
 
-console.log({ guess1 });
+// import { title } from './store.js'
+// import { wordChecker } from './logic.js'
 
-let wordField = document.getElementById("submit-btn");
+// wordChecker('a', 'b');
 
-wordField.addEventListener('click', (e) => {
-
-
-
-})
-
-console.log(wordField);
+// console.log({ title });
